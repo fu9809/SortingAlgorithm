938e52f02c523416c5e5a4e9382984067c81583e
kongm <fyf980921@163.com>
2019-06-06 17:43:03 +0800

    经典算法

---
+++ notes_02_QuickSort/Quick_Classical.java
@@ -0,0 +1,30 @@
+public class Quick_Classical {
+    public static void main(String[] args) {
+        int[] arr = new int[]{6, 9, 28, 13, 31, 26, 1, 16, 7, 22, 49, 46, 45, 46};
+        quick_sort(arr, 0, arr.length-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+", ");
+        }
+    }
+
+    public static void quick_sort(int[] arr, int left, int right){
+        if(left >= right) return;   // 递归出口
+
+        int l = left, r = right;
+        int key = arr[l];
+        while(l < r){
+            while(l < r && key <= arr[r]){
+                r--;
+            }
+            arr[l] = arr[r];
+            while(l < r && key >= arr[l]){
+                l++;
+            }
+            arr[r] = arr[l];
+        }
+        arr[l] = key;
+        quick_sort(arr, left, l-1);
+        quick_sort(arr, l+1, right);
+    }
+}
+
